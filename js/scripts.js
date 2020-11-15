8c70b33b5b0d2ae4ade55a43e80044482e563853
00009065 <73018666+00009065@users.noreply.github.com>
2020-11-15 15:23:31 +0500

    Update scripts.js
    Comments to JS added

--- js/scripts.js
+++ js/scripts.js
@@ -1,58 +1,58 @@
-let tab = function() {
+let tab = function() {  // Get all the classes with needed names and one variable.
     let tabNav = document.querySelectorAll(
         '.tabs-nav__item'),
         tabContent = document.querySelectorAll('.tab'),
         tabName;
 
-    tabNav.forEach(item => {
+    tabNav.forEach(item => { // iterate over the items of tabNav to make them react on click event
         item.addEventListener('click', selectTabNav)
     });
 
-    function selectTabNav() {
+    function selectTabNav() { // choosing tabNav object when any of them clicked
         tabNav.forEach(item => {
             item.classList.remove('is-active')
         });
         console.log(this);
-        this.classList.add('is-active');
-        tabName = this.getAttribute('data-tab-name');
+        this.classList.add('is-active'); // adding class is-active to the pressed tab
+        tabName = this.getAttribute('data-tab-name'); // giving tabName to choose which content to present
         console.log(tabName);
-        selectTabContent(tabName);
+        selectTabContent(tabName); // last function to call
     }
      
-    function selectTabContent(tabName) {
+    function selectTabContent(tabName) { // choosing content depending on a pressed tab
         tabContent.forEach(item => {
-            item.classList.contains(tabName)? item.classList.add('is-active'):
-            item.classList.remove('is-active');
+            item.classList.contains(tabName)? item.classList.add('is-active'): // make needed tab concent active and show to user
+            item.classList.remove('is-active'); // remove is-active from the tab content which doesn't have tabName var in class name
         })
     }
 
 };
 
-tab();
+tab(); // Calling a function
 
 
 'use strict';
     var multiItemSlider = (function () {
       return function (selector, config) {
         var
-          _mainElement = document.querySelector(selector), 
-          _sliderWrapper = _mainElement.querySelector('.slider__wrapper'), 
-          _sliderItems = _mainElement.querySelectorAll('.slider__item'), 
-          _sliderControls = _mainElement.querySelectorAll('.slider__control'), 
-          _sliderControlLeft = _mainElement.querySelector('.slider__control_left'), 
-          _sliderControlRight = _mainElement.querySelector('.slider__control_right'), 
-          _wrapperWidth = parseFloat(getComputedStyle(_sliderWrapper).width), 
-          _itemWidth = parseFloat(getComputedStyle(_sliderItems[0]).width), 
-          _positionLeftItem = 0, 
-          _transform = 0, 
-          _step = _itemWidth / _wrapperWidth * 100, 
-          _items = [], 
-          _interval = 0,
+          _mainElement = document.querySelector(selector), // main element of the block
+          _sliderWrapper = _mainElement.querySelector('.slider__wrapper'), // wrapper of the slider__item
+          _sliderItems = _mainElement.querySelectorAll('.slider__item'), // elements of the slider__item
+          _sliderControls = _mainElement.querySelectorAll('.slider__control'), // controls for the slider
+          _sliderControlLeft = _mainElement.querySelector('.slider__control_left'), // left button
+          _sliderControlRight = _mainElement.querySelector('.slider__control_right'), // right button
+          _wrapperWidth = parseFloat(getComputedStyle(_sliderWrapper).width), // width of the wrapper
+          _itemWidth = parseFloat(getComputedStyle(_sliderItems[0]).width), // width of the one element
+          _positionLeftItem = 0, // position of the left active item
+          _transform = 0, // var of the transform of wrapper .slider__wrapper
+          _step = _itemWidth / _wrapperWidth * 100, // size of the step for transformation
+          _items = [], // array for elements
+          _interval = 0, 
           _config = {
-            isCycling: false,
-            direction: 'right', 
-            interval: 5000, 
-            pause: true 
+            isCycling: false, // automatic change of slides
+            direction: 'right', // direction of the slides change
+            interval: 5000, // interval for slides change
+            pause: true //if to pause when mouse is on slider
           };
 
         for (var key in config) {
@@ -61,7 +61,7 @@ tab();
           }
         }
 
-        
+        // getting values for slider items
         _sliderItems.forEach(function (item, index) {
           _items.push({ item: item, position: index, transform: 0 });
         });
@@ -127,7 +127,7 @@ tab();
           }, _config.interval);
         }
 
-  
+        // processing the comands "back" and "forward"
         var _controlClick = function (e) {
           if (e.target.classList.contains('slider__control')) {
             e.preventDefault();
